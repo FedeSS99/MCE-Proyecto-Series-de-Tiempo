e0c81f7ad242295c4ef9c0e5b5b161661d9fe8c7
Federico Salinas Samaniego <salinasfederico773@gmail.com>
2024-12-07 10:13:43 -0600

    Edit SARIMA file

--- SARIMA.R
+++ SARIMA.R
@@ -8,61 +8,57 @@ datosGA1 <- read.csv("./data/GA1.csv")
 datosSIAP <- read.csv("./data/IVF_SIAP.csv")
 
 # intervalo de tiempo que comparten ene2015-dic2023
-GA1_comp <- ts(datosGA1[265:372,])
-SIAP_comp <- ts(datosSIAP[1:108,])
+GA1 <- ts(datosGA1[265:372, 2], start = 2015, frequency = 12)
+IVF <- ts(datosSIAP[1:108, 2], start = 2015, frequency = 12)
 
 # Visualizacion de GA1 y SIAP entre ene2015-dic2023
-autoplot(GA1_comp[,2], ts.colour = "blue") + ggtitle("GA1")
-autoplot(SIAP_comp[,2], ts.colour = "black") + ggtitle("SIAP")
+autoplot(GA1, ts.colour = "blue") + ggtitle("GA1")
+autoplot(IVF, ts.colour = "black") + ggtitle("SIAP")
 
 # Graficas ACF Y PCF de series originales
 par(mfrow = c(2, 1))
-acf(GA1_comp[, 2])
-pacf(GA1_comp[, 2])
+acf(GA1)
+pacf(GA1)
 
 par(mfrow = c(2, 1))
-acf(SIAP_comp[, 2])
-pacf(SIAP_comp[, 2])
-
-# Transformacion logaritmica base 10 y Diferenciacion con lag = 12 (Posible overfitting)
-tranGA1 <- diff(log10(GA1_comp[,2]), lag = 12)
-tranSIAP <- diff(log10(SIAP_comp[,2]), lag = 12)
-
-# Visualizamos las series bajo la transformacion y diferenciacion
-autoplot(tranGA1, ts.colour = "blue") + ggtitle("GA1")
-autoplot(tranSIAP, ts.colour = "black") + ggtitle("SIAP")
-
-# Graficas ACF Y PCF de series transformadas
-par(mfrow = c(2, 1))
-acf(tranGA1)
-pacf(tranSIAP)
-
-par(mfrow = c(2, 1))
-acf(tranGA1)
-pacf(tranSIAP)
+acf(IVF)
+pacf(IVF)
 
 # Tests aumentados de Dickey-Fuller en ambas series
-adf_testAG1 <- adf.test(tranGA1, alternative = "stationary")
-adf_testAG1
-# p-valor = 0.02 < 0.05 -> Evidencia de estacionariedad
+adf_testGA1comp <- adf.test(GA1, alternative = "stationary")
+adf_testGA1comp
+# p-valor = menor a 0.01 -> Evidencia de estacionariedad
 
-adf_testSIAP <- adf.test(tranSIAP, alternative = "stationary")
-adf_testSIAP
-# p-valor = 0.045 < 0.05 -> Evidencia de estacionariedad (dudoso)
+adf_testSIAPcomp <- adf.test(IVF, alternative = "stationary")
+adf_testSIAPcomp
+# p-valor = 0.0153 -> Evidencia de estacionariedad
 
 # Variables predictoras y objetivo
-N <- nrow(GA1_comp)
-X <- SIAP_comp[1:(N - 1), 2]
-Y <- GA1_comp[1:(N - 1), 2]
-Xpresente <- SIAP_comp[N, 2]
-Ypresente <- GA1_comp[N, 2]
+N <- length(GA1)
+X <- IVF[1:(N - 1)]
+Y <- GA1[1:(N - 1)]
+Xpresente <- IVF[N]
+Ypresente <- GA1[N]
+
+# Ajuste de modelo ARIMA
+arima_model <- auto.arima(Y, xreg = X, max.p = 12, max.q = 12, stepwise = FALSE, nmodels = 256, approximation = FALSE, stationary = TRUE, seasonal = FALSE, parallel = TRUE, num.cores = NULL)
+summary(arima_model)
+# Revisamos los residuales del modelo ARIMA ajustado
+checkresiduals(arima_model)
 
 # Ajuste de modelo SARIMA
-sarima_model <- Arima(Y, order = c(12, 0, 0), xreg = X)
+sarima_model <- auto.arima(Y, xreg = X, max.p = 12, max.q = 12, max.P = 12, max.Q = 12, stepwise = FALSE, nmodels = 256, approximation = FALSE, stationary = TRUE, seasonal = TRUE, parallel = TRUE, num.cores = NULL)
 summary(sarima_model)
 # Revisamos los residuales del modelo SARIMA ajustado
 checkresiduals(sarima_model)
 
+# Realizamos nowcasting con ARIMA
+GA1_presente_ARIMA <- forecast(arima_model, xreg = Xpresente, h = 1)
+GA1_presente_ARIMA
+par(mfrow = c(1,1))
+plot(GA1_presente_ARIMA)
+
+# Realizamos nowcasting con SARIMA
 GA1_presente_SARIMA <- forecast(sarima_model, xreg = Xpresente, h = 1)
 GA1_presente_SARIMA
 par(mfrow = c(1,1))
