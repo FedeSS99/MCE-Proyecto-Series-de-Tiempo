f62c575092dd727bee88703b76c73d0dba03d7d7
Federico Salinas Samaniego <salinasfederico773@gmail.com>
2024-12-09 12:10:35 -0600

    Updated error comparison file

--- Comprobacion.R
+++ Comprobacion.R
@@ -1,6 +1,17 @@
+source("./FuncionesErrores.R")
+
 ResSARIMA <- read.csv("./data/nowSARIMA.csv")
 ResDFM <- read.csv("./data/nowDFM.csv")
 
+# -- Calculo del MAE y MSE para los modelos SARIMA y DMF --
+Errores_SARIMA <- calcular_MAE_MSE(ResSARIMA$nowSARIMA, ResSARIMA$Obs)
+rownames(Errores_SARIMA) <- c("SARIMA")
+Errores_DFM <- calcular_MAE_MSE(ResDFM$nowDFM, ResDFM$Obs)
+rownames(Errores_DFM) <- c("DFM")
+Errores <- rbind(Errores_SARIMA, Errores_DFM)
+Errores
+
+# -- Obtención de residuales por modelo para comparar visualmente y mediante el test de Diebold-Mariano --
 errorSARIMA <- ResSARIMA$nowSARIMA - ResSARIMA$Obs
 errorDFM <- ResDFM$nowDFM - ResDFM$Obs
 
